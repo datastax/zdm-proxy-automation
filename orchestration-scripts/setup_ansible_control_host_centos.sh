7e5cca690c28d9d7db292c83c5f6aebabf350439
Alice Lottini <alice.lottini@datastax.com>
2021-09-27 21:28:05 +0100

    Added script to set up Ansible control host when using a Centos machine.

---
+++ orchestration-scripts/setup_ansible_control_host_centos.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#####################################################################################
+### Prerequisites ###
+### Please make sure that the following files exist and are in the correct location:
+
+###  - ~/.ssh/<cloudgate_keypair_name>
+###  - ~/.ssh/<cloudgate_keypair_name>.pub
+###  - ~/.ssh/cloudgate-automation-deploy-key
+###  - ~/cloudgate-inventory
+
+#####################################################################################
+
+###################################################
+### Configuration variables
+###################################################
+
+# REQUIRED variables - Please uncomment and specify
+#cloudgate_keypair_name=
+
+# OPTIONAL variables - Please change defaults as appropriate
+ssh_dir="/home/centos/.ssh"
+cloudgate_vpc_cidr_first_two_octets="172.18"
+
+###################################################
+### Main script
+###################################################
+
+# Change the permissions of the Cloudgate key pair
+chmod 400 "${ssh_dir}"/"${cloudgate_keypair_name}"*
+
+#Ensure that the ssh agent is running
+eval $(ssh-agent -s)
+
+# Add the private ssh key to the ssh agent
+ssh-add "${ssh_dir}"/"${cloudgate_keypair_name}"
+
+# Append the following lines to the ssh config file (creating it if it doesn't exist)
+printf "# proxy instances \nHost %s.*\n  IdentityFile %s/%s\n" "${cloudgate_vpc_cidr_first_two_octets}" "${ssh_dir}" "${cloudgate_keypair_name}" >> "${ssh_dir}"/config
+
+# Install Ansible if it has not already been installed
+if ! command -v ansible &> /dev/null; then
+  echo "Installing Ansible"
+  sudo yum install epel-release
+  sudo yum install ansible
+fi
+
+# Install the jmespath dependency
+sudo yum install python2-jmespath
+
+# Install the community.docker dependency
+ansible-galaxy collection install community.docker
+
+# Install the community.general dependency
+ansible-galaxy collection install community.general
+
+# Install git
+sudo yum install git
+
+# Set up the Cloudgate repository deploy key
+chmod 400 "${ssh_dir}"/cloudgate-automation-deploy-key
+
+# Append the following lines to the ssh config file (creating it if it doesn't exist)
+printf "# deploy key \nHost cloudgate-automation github.com\n  Hostname github.com\n  IdentityFile %s/cloudgate-automation-deploy-key\n"  "${ssh_dir}" >> "${ssh_dir}"/config
+
+# Change permissions of ssh config file
+chmod 600 "${ssh_dir}"/config
+
+# Clone the automation repo
+git clone git@cloudgate-automation:riptano/cloudgate-automation.git
+
+# Put the inventory file into the ansible directory of the cloudgate automation code
+mv /home/centos/cloudgate_inventory /home/centos/cloudgate-automation/ansible
