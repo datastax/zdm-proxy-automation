87d80c7fd2b262ede21820f9889883237076c562
Alice Lottini <alice.lottini@datastax.com>
2022-07-27 13:24:29 +0100

    Fixed the script that initialises the Ansible container internally. Removed the shell script to prompt for input, create and configure the container from the host as it has been superseded by the Golang utility

--- ansible-control-host-docker/init_container_internal.sh
+++ ansible-control-host-docker/init_container_internal.sh
@@ -1,70 +1,125 @@
 #!/bin/bash
 
+# Help function
+print_help_message () {
+  echo
+  echo
+  printf "Usage: ./init_container_internal.sh [ARG] [ARG], where ARG has the form: arg_name arg_value \n"
+  echo
+  printf "Required arguments: \n"
+  printf "  -p / --proxy_ip_address_prefix: common prefix of the ip addresses of all proxy instances \n"
+  printf "  -i / --ansible_inventory_name: name of the Ansible inventory file, which must be located in the home directory of the container \n"
+  echo
+  printf "Examples:  \n"
+  printf "  ./init_container_internal.sh -p 172.18.* -i my_ansible_inventory \n"
+  printf "  ./init_container_internal.sh -proxy_ip_address_prefix 172.18.* -ansible_inventory_name my_ansible_inventory \n"
+  printf "  ./init_container_internal.sh --proxy_ip_address_prefix 172.18.* --ansible_inventory_name my_ansible_inventory \n"
+  printf "Short and long options can also be combined \n"
+  echo
+  printf "This script will exit now, please try again. \n"
+  echo
+  echo
+  return
+}
+
+#### Main script ####
+
+# Exit if any command fails
+set -e
 SSH_KEY_DIR="/home/ubuntu/zdm-proxy-ssh-key-dir/"
 echo "****************************************************************** "
 echo "*** This script initializes the Ansible Control Host container *** "
 echo "****************************************************************** "
 echo
 
-echo "Please enter the common prefix of the private IP addresses of the proxy instances. Simply press ENTER to use the default of 172.18.* "
-read -r PROXY_PRIVATE_IP_PREFIX
+# Parse named command-line arguments
+SHORT=p:,i:,h
+LONG=proxy_ip_address_prefix:,ansible_inventory_name:,help
+OPTS=$(getopt -a -n init_container_internal --options $SHORT --longoptions $LONG -- "$@")
 
-if [ -z "$PROXY_PRIVATE_IP_PREFIX" ]
-then
-  PROXY_PRIVATE_IP_PREFIX="172.18.*"
-fi
-echo
+VALID_ARGUMENTS=$#
 
-echo "Please enter the path and name of your Ansible inventory file on the container. Simply press ENTER if your inventory is /home/ubuntu/cloudgate_inventory"
-read -r ANSIBLE_INVENTORY
-
-if [ -z "$ANSIBLE_INVENTORY" ]
-then
-  ANSIBLE_INVENTORY="/home/ubuntu/cloudgate_inventory"
+if [ "$VALID_ARGUMENTS" -eq 0 ]; then
+  echo "ERROR: Missing mandatory parameters"
+  print_help_message
+  exit 1
 fi
 
-if [ ! -f $ANSIBLE_INVENTORY ]
+eval set -- "$OPTS"
+while :
+do
+  case "$1" in
+    -p | --proxy_ip_address_prefix )
+      PROXY_IP_ADDRESS_PREFIX="$2"
+      shift 2
+      ;;
+    -i | --ansible_inventory_name )
+      ANSIBLE_INVENTORY_NAME="$2"
+      shift 2
+      ;;
+    -h | --help )
+      print_help_message
+      exit 1
+      ;;
+    --)
+      shift;
+      break
+      ;;
+    * )
+      echo "Unknown option: $1"
+      print_help_message
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$PROXY_IP_ADDRESS_PREFIX" ]
 then
-    echo "The Ansible inventory file $ANSIBLE_INVENTORY was not found. Please ensure that the file exists and is located in the specified path on the container."
-    echo "To copy your Ansible inventory from the host to the container, open a shell on the host and run the following command, removing the angle brackets and replacing the values appropriately: "
-    echo "sudo docker cp <your-ansible-inventory-name> <container-name>:/home/ubuntu/ "
-    echo "Then run this script again"
-    return
+  echo "ERROR: Missing mandatory parameter proxy_ip_address_prefix"
+  print_help_message
+  exit 1
 fi
 
-echo
+if [ -z "$ANSIBLE_INVENTORY_NAME" ]
+then
+  echo "ERROR: Missing mandatory parameter ansible_inventory_name"
+  print_help_message
+  exit 1
+fi
 
+#Copy all provided keys to the .ssh directory, change permissions and update the ssh config file accordingly
 if [ "$(ls -A $SSH_KEY_DIR)" ]; then
    cd $SSH_KEY_DIR || return
    for KEY_FILE in *
    do
      echo "Copying key $KEY_FILE to the SSH directory"
-     chmod 400 "$KEY_FILE"
+     sudo chmod 400 "$KEY_FILE"
      sudo cp "$KEY_FILE" /home/ubuntu/.ssh/
-     if pcregrep -Mq "Host $PROXY_PRIVATE_IP_PREFIX\n  IdentityFile /home/ubuntu/.ssh/$KEY_FILE" /home/ubuntu/.ssh/config
+     if pcregrep -Mq "Host $PROXY_IP_ADDRESS_PREFIX\n  IdentityFile /home/ubuntu/.ssh/$KEY_FILE" /home/ubuntu/.ssh/config
      then
-       echo "Entry for key $KEY_FILE and proxy IP prefix $PROXY_PRIVATE_IP_PREFIX already exists in the SSH config file"
+       echo "Entry for key $KEY_FILE and proxy IP prefix $PROXY_IP_ADDRESS_PREFIX already exists in the SSH config file"
      else
-       echo "Adding an entry for key $f and proxy IP prefix $PROXY_PRIVATE_IP_PREFIX to the SSH config file"
-       printf "# proxy instances \nHost $PROXY_PRIVATE_IP_PREFIX\n  IdentityFile /home/ubuntu/.ssh/%s\n" "$f" >> /home/ubuntu/.ssh/config
+       echo "Adding an entry for key $f and proxy IP prefix $PROXY_IP_ADDRESS_PREFIX to the SSH config file"
+       printf "# proxy instances \nHost $PROXY_IP_ADDRESS_PREFIX\n  IdentityFile /home/ubuntu/.ssh/%s\n" "$f" >> /home/ubuntu/.ssh/config
      fi
      echo
    done
 else
   echo "No SSH key was found in $SSH_KEY_DIR. At least one SSH key able to access the proxy instances must be made available to the container."
-  echo "To copy keys from the host into $SSH_KEY_DIR on the container, open a shell on the host and run the following command, removing the angle brackets and replacing the values appropriately: "
-  echo "sudo docker cp <your-ssh-key-name> <container-name>:$SSH_KEY_DIR "
-  echo "Then run this script again"
-  return
+  exit 1
 fi
 
 echo "Changing ownership of all keys to user ubuntu"
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh/
 echo
 
-cd
+cd || return
 
-if [ -d "home/ubuntu/cloudgate-automation" ]
+# Add github.com to the known hosts to suppress confirmation prompt when cloning the automation project
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# Clone the automation if it is not already present
+if [ -d "/home/ubuntu/cloudgate-automation" ]
 then
   echo "The automation git repo has already been cloned."
 else
@@ -80,9 +135,12 @@ echo "Setting the appropriate options in the global Ansible configuration file"
 printf "[ssh_connection]\nssh_args = -o StrictHostKeyChecking=no\n" > ansible.cfg
 echo
 
+cd || return
+
+# Copy the Ansible inventory into the Ansible automation directory
 echo "Making the inventory file available to Ansible"
-sudo chown ubuntu:ubuntu $ANSIBLE_INVENTORY
-mv $ANSIBLE_INVENTORY /home/ubuntu/cloudgate-automation/ansible
+sudo chown ubuntu:ubuntu "$ANSIBLE_INVENTORY_NAME"
+mv "$ANSIBLE_INVENTORY_NAME" /home/ubuntu/cloudgate-automation/ansible
 echo
 
 echo "************************************************************************ "
@@ -90,5 +148,5 @@ echo "*** The Ansible container is now fully initialized and ready to use. *** "
 echo "************************************************************************ "
 echo
 echo "You can proceed to configure and run the Ansible playbooks to deploy and manage the proxies."
-echo "As a reminder, your Ansible inventory file is called $ANSIBLE_INVENTORY and is now located in cloudgate-automation/ansible"
+echo "As a reminder, your Ansible inventory file is called $ANSIBLE_INVENTORY_NAME and is now located in cloudgate-automation/ansible"
 
