0d7618e1e86b1ce6974ef4c50d763a83fabe3929
weideng1 <weideng1@users.noreply.github.com>
2022-12-07 07:56:15 -0700

    ZDM-489 ubuntu 22.04 upgrade #26 (#45)

--- terraform/aws/submodules-aws/instances-aws/variables.tf
+++ terraform/aws/submodules-aws/instances-aws/variables.tf
@@ -9,19 +9,23 @@ variable "ami" {
   description = "AMI to be used for this EC2 instance"
   type = map(string)
   default = {
-    "us-east-1" = "ami-0bb5a904861ec247f"
-    "us-east-2" = "ami-025227bc8f4c2cbdd"
-    "us-west-1" = "ami-0558dde970ca91ee5"
-    "us-west-2" = "ami-0bdef2eb518663879"
-
-    "eu-west-1" = "ami-0c259a97cbf621daf"
-    "eu-west-2" = "ami-013fadefd0ab548ef"
-    "eu-west-3" = "ami-0e34a9addd905132f"
-    "eu-central-1" = "ami-073375fc9e17516d6"
-    "eu-north-1" = "ami-09f07498568488979"
-    "eu-south-1" = "ami-016e7e862dbd5eb59"
-
-    "ap-south-1" = "ami-07a4716f9f312e466"
+    "ap-south-1" = "ami-07ffb2f4d65357b42"
+    "ap-south-2" = "ami-0d8d9a2de1bcdb066"
+    "ap-southeast-1" = "ami-02045ebddb047018b"
+    "ap-southeast-2" = "ami-0df609f69029c9bdb"
+    "ap-southeast-3" = "ami-030e837d78f47cbb1"
+    "eu-central-1" = "ami-06ce824c157700cd2"
+    "eu-central-2" = "ami-0876b3d2f699dd5f3"
+    "eu-north-1" = "ami-0fd303abd14827300"
+    "eu-south-1" = "ami-03533c29d03faff35"
+    "eu-south-2" = "ami-06a35994aaed77af8"
+    "eu-west-1" = "ami-05e786af422f8082a"
+    "eu-west-2" = "ami-07c2ae35d31367b3e"
+    "eu-west-3" = "ami-03b755af568109dc3"
+    "us-east-1" = "ami-0574da719dca65348"
+    "us-east-2" = "ami-0283a57753b18025b"
+    "us-west-1" = "ami-0a1a70369f0fce06a"
+    "us-west-2" = "ami-0ecc74eca1d66d8a6"
   }
 }
 
